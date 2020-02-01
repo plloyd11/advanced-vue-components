9e645cff5d0d01ff94dc79a5619af394a135c39e
plloyd11 <plloyd11@gmail.com>
2020-02-01 17:13:18 -0500

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="min-h-screen bg-grey-darker p-8">
+    <div class="max-w-md mx-auto">
+      <user-settings-form></user-settings-form>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserSettingsForm from "@/components/UserSettingsForm.vue";
+
+export default {
+  name: "home",
+  components: {
+    UserSettingsForm
+  }
+};
+</script>
